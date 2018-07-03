479d6cb65e4071f1944d7daa0eaa23198e0a41fa
vhetet <vhetet@solstice.com>
2018-07-03 14:51:44 -0500

    proof that openAndActivate works

--- CefSharpRevitExperimentation/Class1.cs
+++ CefSharpRevitExperimentation/Class1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
@@ -18,51 +20,16 @@ namespace CefSharpRevitExperimentation
     public class Class1
         : IExternalCommand
     {
-        MainPage m_MyDockableWindow = null;
 
         public Result Execute(
             ExternalCommandData commandData,
             ref string message,
             ElementSet elements)
         {
-            DockablePaneProviderData data
-                = new DockablePaneProviderData();
-
-            MainPage MainDockableWindow = new MainPage();
-
-            m_MyDockableWindow = MainDockableWindow;
-
-            data.FrameworkElement = MainDockableWindow
-                as System.Windows.FrameworkElement;
-
-            data.InitialState = new DockablePaneState();
-
-            data.InitialState.DockPosition
-                = DockPosition.Right;
-
-            data.InitialState.TabBehind = DockablePanes
-                .BuiltInDockablePanes.ProjectBrowser;
-
-            DockablePaneId dpid = new DockablePaneId(
-                new Guid("{D7C963CE-B7CA-426A-8D51-6E8254D21157}"));
-
-            commandData.Application.RegisterDockablePane(
-                dpid, "AEC Dockable Window", MainDockableWindow
-                    as IDockablePaneProvider);
-
-            commandData.Application.ViewActivated
-                += new EventHandler<ViewActivatedEventArgs>(
-                    Application_ViewActivated);
-
+//            Process.Start(@"C:\Users\Vincent\source\repos\usg-tech-feasibility-pocs\webkit-addin-poc\web\sample001.rvt");
+//            UIDocument test = new UIDocument(new Document());
+            commandData.Application.OpenAndActivateDocument(@"C:\Users\Vincent\source\repos\usg-tech-feasibility-pocs\webkit-addin-poc\web\sample001.rvt");
             return Result.Succeeded;
-        }
-
-        void Application_ViewActivated(
-            object sender,
-            ViewActivatedEventArgs e)
-        {
-            m_MyDockableWindow.lblProjectName.Content
-                = e.Document.ProjectInformation.Name;
         }
     }
 }
